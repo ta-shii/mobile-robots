f2af6d6fc210be80f28a3f3e8194820a5e980809
ta-shii <tashichimilhamo@gmail.com>
2026-04-01 18:09:26 +0800

    feat: ROS2 workpspace-initialize p3at package with obstacle avoidance node and necessary configurations

---
+++ ros2_ws/src/p3at/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'p3at'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tashi',
+    maintainer_email='tashichimilhamo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'simple_avoid = p3at.simple_avoid:main',
+        ],
+    },
+)
